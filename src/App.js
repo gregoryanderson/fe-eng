552c45aa6fb154610acefd5ddedd5a5cc81d3df5
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-07-01 22:03:54 -0600

    Style first table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Nav from '../src/components/Nav'
+import Table from '../src/components/Table'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: [],
+      merchants: [],
+      items: [],
+      error: "",
+    };
+  }
+
+  async componentDidMount() {
+    fetch("https://back-engine.herokuapp.com/api/v1/customers")
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("There was an error");
+        }
+        return response.json();
+      })
+      .then((allData) => {
+        this.setState({ customers: allData.data });
+      }).catch(err => {
+        throw Error(err)
+      })
+    
+      fetch("https://back-engine.herokuapp.com/api/v1/merchants")
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("There was an error");
+        }
+        return response.json();
+      })
+      .then((allData) => {
+        this.setState({ merchants: allData.data });
+      }).catch(err => {
+        throw Error(err)
+      })
+
+      fetch("https://back-engine.herokuapp.com/api/v1/items")
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("There was an error");
+        }
+        return response.json();
+      })
+      .then((allData) => {
+        this.setState({ items: allData.data });
+      }).catch(err => {
+        throw Error(err)
+      })
+  }
+
+  render() {
+    return (
+      <>
+        <Nav />
+        {this.state.customers && <Table customers={this.state.customers}/>}
+      </>
+    );
+  }
 }
 
 export default App;
